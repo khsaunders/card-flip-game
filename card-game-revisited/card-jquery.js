43f47cb2d17fbd76e3f2f05c6baa5f91de2e8435
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2016-06-06 05:39:06 -0400

    ALL OF THE BROKEN STUFF

--- card-game-revisited/card-jquery.js
+++ card-game-revisited/card-jquery.js
@@ -2,7 +2,7 @@ $(function(){
   'use strict';
 });
 
-//timer function//
+//Timer Function Attempt 1//
 // this code provided in a Stackoverflow forum. user nhahtdh provided this response. http://stackoverflow.com/questions/6341774/keep-track-of-how-much-time-is-spent-showing-certain-elements-on-the-page.
 
 // var start = newDate();
@@ -21,10 +21,9 @@ $(function(){
 //     return (elapsed);
 // }
 
-// Did not work. Need to define newDate
-//Realized should be 'new Date()' because date is a built in JS tool.
+//Did not work
 
-//Attempt 2
+//Attempt 2 Card Flipping
 
 // $(".time-count").load(function()
 // this.getTime
@@ -37,15 +36,61 @@ $(function(){
 //
 // });
 
+//TIMER
+
+var count = 0
+var counter = setInterval(timer, 1000);//tells it to count by 1 second (or 1000 milliseconds)
+
+function timer (){
+  count += 1;
+  if (count == 240000) //this is the end point. 4 mins.
+
+  clearInterval(counter);
+}
+
+document.getElementById('time-count').innerHTML= count;
+
+
+
+//CARD FLIPPING
+
 var track = 0;
 
-$('li').click (function(){
+$('li').click (function(flip){
 track++
-if (track <= 2){
-  $(this).addClass('flipped')
-  $(this).children().css('opacity', '1');
+
+  if (track === 1) {
+  $(this).addClass('flipped');
+  $(this).children().css('opacity', '1'); //lets first card flip
   console.log(track);
   }
+
+  else if (track % 2 === 0) {
+    $(this).addClass('flipped');
+    $(this).children().css('opacity', '1')//lets second card flip
+    console.log(track);
+  }
+
+  else if (track % 2 === 0 && $(this).attr('value') !== $('li').attr('value')) {
+  $(this).removeClass('flipped');
+  console.log(track);
+
+}
+
+  else {
+
+
+  }
+
+//   else ($(this).attr('value') === $('this').attr('value')){
+//   $(this).removeClass('flipped')
+//   $(this).addClass('permanent');
+// }
+  });
+
+
+  // $(this).attr('value') === $('li').attr('value')) {
+
 
   // else if (track === 2) {
   // $(this).addClass('flipped')
@@ -57,18 +102,20 @@ if (track <= 2){
   // $(this).removeClass('flipped')
   // $(this).addClass('permanent');
 
-else if (track === 2 && ($(this).attr('value') === $('li').attr('value'))){
-  $(this).removeClass('flipped');
-  $(this).addClass('permanent')
 
-}
+  // else if (track === 2 && $(this).attr('value') !== $('li').attr('value')) {
+  // $(this).removeClass('flipped');
+  // $(this).addClass('no-match')
+  // console.log(track);
 
-else {
-  $(this).removeClass('flipped')
-  $(this).addClass('card')
-  $(this).children().css('opacity', '0');
-}
-});
+// }
+//
+// else {
+//   $(this).removeClass('flipped')
+//   $(this).addClass('permanent')
+//   $(this).children().css('opacity', '0');
+// }
+
 
 
 var hi = 'hello'
